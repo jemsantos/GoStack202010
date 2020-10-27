84d5e83175e26766821e33fc57043b97e1b30a4b
jemsantos <eustaquio.cadastros@gmail.com>
2020-10-26 22:40:27 -0300

    Codigo final da aula: Tipos de Parametros

--- nivel_01/back-end_com_node-js/src/index.js
+++ nivel_01/back-end_com_node-js/src/index.js
@@ -1,8 +1,19 @@
+const { json } = require('express');
 const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  //const query = request.query;
+  // desestruturando
+  const { title, owner } = request.query;
+
+  //console.log(query);
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2'
@@ -10,6 +21,12 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+  //const body = request.body;
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -18,6 +35,11 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+  //const params = request.params;
+  const { id } = request.params;
+
+  console.log(id);
+
   return response.json([
     'Projeto 4',
     'Projeto 2',
