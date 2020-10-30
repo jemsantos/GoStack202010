b07c5c38d2fa898755d6b9319d875ff4e2bfb995
jemsantos <eustaquio.cadastros@gmail.com>
2020-10-29 22:04:43 -0300

    Codigo final da aula: Propriedades

--- nivel_01/font-end_com_react-js/src/components/Header.js
+++ nivel_01/font-end_com_react-js/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Header() {
+export default function Header({ title, children }) { /* props */
   return(
     <header>
-      <h1>ReactJS</h1>
+      <h1>{title}</h1>
+
+      {children}
     </header>
-  );
+  ); /* <h1>{props.title}</h1> */
 }
