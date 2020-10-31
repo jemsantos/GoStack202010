5c0dde1121eeb8b7caf503d70b0798cb5073a1e5
jemsantos <eustaquio.cadastros@gmail.com>
2020-10-30 23:47:55 -0300

    Código final da aula: Listando Projetos da API

--- nivel_01/mobile_com_react_native/src/index.js
+++ nivel_01/mobile_com_react_native/src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#7159C1" />
       
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+        {/* <View style={styles.container}>
+          {projects.map(project => (
+            <Text key={project.id} style={styles.project}>{project.title}</Text>
+          ))}
+          </View> */}
+      </SafeAreaView>
     </>
   );
 };
@@ -17,12 +39,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159C1',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 32,
-    fontWeight: 'bold'
+    fontSize: 20
   }
 });
