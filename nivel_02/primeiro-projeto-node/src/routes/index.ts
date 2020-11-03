0444cd28309b26e838346ec74a5ed62378faab78
jemsantos <eustaquio.cadastros@gmail.com>
2020-11-02 21:45:49 -0300

    Codigo final da aula: "Construindo Aplicação: Services & SOLID"

--- nivel_02/primeiro-projeto-node/src/routes/index.ts
+++ nivel_02/primeiro-projeto-node/src/routes/index.ts
@@ -3,19 +3,6 @@ import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-/* transfere todas as rotas iniciadas com "appointmensts" para o "appointmentsRouter" */
 routes.use('/appointmensts', appointmentsRouter);
-
-/*
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-}); */
 
 export default routes;
---
+++ nivel_02/primeiro-projeto-node/src/services/CreateAppointmentService.ts
@@ -0,0 +1,45 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * Recebimento das informações
+ * Trativa de erros/excessões
+ * Acesso ao repositório
+ */
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  /** Dependency Inversion (SOLID) */
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
