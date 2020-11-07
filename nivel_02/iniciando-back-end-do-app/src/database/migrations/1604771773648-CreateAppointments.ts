bb21b52be6a7d64d6ee5923ddfbcfe73a35d6790
jemsantos <eustaquio.cadastros@gmail.com>
2020-11-07 16:56:28 -0300

    Código final da aula: Repositório do TypeORM

--- nivel_02/iniciando-back-end-do-app/src/database/migrations/1604771773648-CreateAppointments.ts
+++ nivel_02/iniciando-back-end-do-app/src/database/migrations/1604771773648-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1604771773648
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            /* default: 'uuid_generate_v4()',  ==> ISTO E PARA O POSTGREE */
           },
           {
             name: 'provider',
--- nivel_02/iniciando-back-end-do-app/src/routes/appointments.routes.ts
+++ nivel_02/iniciando-back-end-do-app/src/routes/appointments.routes.ts
@@ -1,29 +1,29 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const creteApponitment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+    const creteApponitment = new CreateAppointmentService();
 
-    const appointment = creteApponitment.execute({
+    const appointment = await creteApponitment.execute({
       date: parsedDate,
       provider,
     });
