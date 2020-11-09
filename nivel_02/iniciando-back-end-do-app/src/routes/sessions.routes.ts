f887907a06db1cc370003a2d66487bd22b41dd91
jemsantos <eustaquio.cadastros@gmail.com>
2020-11-09 18:49:32 -0300

    Código final da aula: Gerando token JWT

--- nivel_02/iniciando-back-end-do-app/src/routes/sessions.routes.ts
+++ nivel_02/iniciando-back-end-do-app/src/routes/sessions.routes.ts
@@ -5,18 +5,19 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
   try {
+    const { email, password } = request.body;
+
     const authenticateUser = new AuthenticateUserService();
 
-    const { user } = await authenticateUser.execute({
+    const { user, token } = await authenticateUser.execute({
       email,
       password,
     });
 
     delete user.password;
 
-    return response.json(user);
+    return response.json({ user, token });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
