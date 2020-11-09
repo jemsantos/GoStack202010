26338cd351483b24b43cec1f87075784901fb45e
jemsantos <eustaquio.cadastros@gmail.com>
2020-11-08 23:00:13 -0300

    Código final da aula: Conceitos de JWT

--- nivel_02/iniciando-back-end-do-app/src/routes/index.ts
+++ nivel_02/iniciando-back-end-do-app/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointmensts', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
