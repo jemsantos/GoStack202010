f38ba07ca41c474ca81ef80f90448451ea78b687
jemsantos <eustaquio.cadastros@gmail.com>
2020-11-22 23:09:24 -0300

    Codigo final da aula: Isolando componentes

--- nivel_03/iniciando_front-end_web/src/pages/Signin/styles.ts
+++ nivel_03/iniciando_front-end_web/src/pages/Signin/styles.ts
@@ -3,18 +3,6 @@ import { shade } from 'polished';
 
 import signInBackgroundImg from '../../assets/sign-in-background.png';
 
-/* import { shade } from 'polished'; // , { css } */
-
-/* interface FormProps {
-  hasError: boolean;
-} */
-/*
-  font-size: 48px;
-  color: #3a3a3a;
-  max-width: 450px;
-  line-height: 56px;
-  margin-top: 80px;
-*/
 export const Container = styled.div`
   height: 100vh;
 
@@ -38,40 +26,6 @@ export const Content = styled.div`
 
     h1 {
       margin-bottom: 24px;
-    }
-
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312e38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
     }
 
     a {
