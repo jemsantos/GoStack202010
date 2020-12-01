05a2fded80c000d8c8bbea3e82fe7f0d0b25c38f
jemsantos <eustaquio.cadastros@gmail.com>
2020-12-01 16:14:19 -0300

    Codigo final da aula: Tela de cadastro

--- nivel_03/appgobarber/src/routes/index.tsx
+++ nivel_03/appgobarber/src/routes/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../pages/SignIn';
@@ -10,13 +9,10 @@ const Auth = createStackNavigator();
 const AuthRoutes: React.FC = () => (
   <Auth.Navigator
     screenOptions={{
-       headerShown: false,
-      /* headerTintColor: '#fff',
-      headerStyle: {
-        backgroundColor: '#7159c1',
-      }, */
+      headerShown: false,
       cardStyle: { backgroundColor: '#312e38' }
     }}
+    /* initialRouteName="SignUp" */
   >
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
