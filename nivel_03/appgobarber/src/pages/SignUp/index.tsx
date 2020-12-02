e7a2222fa549b3005cf7fa06761c29719b131cb6
jemsantos <eustaquio.cadastros@gmail.com>
2020-12-01 21:47:17 -0300

    Codigo final da aula: Cadastro na aplicação

--- nivel_03/appgobarber/src/pages/SignUp/index.tsx
+++ nivel_03/appgobarber/src/pages/SignUp/index.tsx
@@ -14,6 +14,8 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -58,9 +60,14 @@ const SignUp: React.FC = () => {
           abortEarly: false,
         });
 
-        /* await api.post('/users', data);
+        await api.post('/users', data);
 
-        history.push('/'); */
+        Alert.alert(
+          'Cadastro realizado com sucesso!',
+          'Você já pode fazer login na aplicação.'
+        );
+
+        navigation.goBack(); // navigation.navigate('SignIn');
       } catch (error) {
         if (error instanceof Yup.ValidationError) {
           const errors = getValidationErrors(error);
