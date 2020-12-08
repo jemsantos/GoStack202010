7655a6ec47c9b31942eac7297836353acb57acf8
jemsantos <eustaquio.cadastros@gmail.com>
2020-12-07 23:01:40 -0300

    Codigo final da aula: Provider de storage

--- nivel_04/iniciando-back-end-do-app/src/modules/users/services/UpdateUserAvatarService.ts
+++ nivel_04/iniciando-back-end-do-app/src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -18,6 +19,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -28,19 +32,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAavatarFilePath = path.join(
-        uploadConfig.directory,
-        user.avatar,
-      );
-
-      const userAvatarFileExists = await fs.promises.stat(userAavatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAavatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
