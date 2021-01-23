ab8745ec2c253a939cd52ede8d25a3fe0e5f8ea0
jemsantos <eustaquio.cadastros@gmail.com>
2021-01-23 18:59:40 -0300

    Código final gerado no módulo 03

---
+++ nivel_05/finalizando-front-end-web-do-app/src/routes/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+
+import SigIn from '../pages/SignIn';
+import SigUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SigIn} />
+    <Route path="/signup" exact component={SigUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
+  </Switch>
+);
+
+export default Routes;
