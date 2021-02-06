9b154950aaf16ae80b9f8d8e6f65fe5525d42fae
jemsantos <eustaquio.cadastros@gmail.com>
2021-02-06 14:38:10 -0300

    Codigo final da aula: M2-Mobile - Sucesso - Criando estrutura da tela

--- nivel_05/finalizando-front-end-mobile-do-app/appgobarber/src/pages/AppointmentCreated/index.tsx
+++ nivel_05/finalizando-front-end-mobile-do-app/appgobarber/src/pages/AppointmentCreated/index.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useNavigation } from '@react-navigation/native';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        { name: 'Dashboard', },
+      ],
+      index: 0,
+    });
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-    </View>
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 14 de março de 2020 às 12:00h</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
   );
 };
 
